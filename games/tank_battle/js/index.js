fa2985aa754e9359a331c24257dd64a74f760df5
jiabin.li <jiabin.li@shopee.com>
2022-08-10 19:18:55 +0800

    update

--- games/tank_battle/js/index.js
+++ games/tank_battle/js/index.js
@@ -1,12 +1,17 @@
-import loader, { imgs } from "./engine/loader.js";
+import loader from "./engine/loader.js";
 import ctx from "./engine/renderer.js";
 
 import Map from "./role/Map.js"
 
-function render() {
-  const round = new Map(12);
-  round.draw(ctx, 0, 0);
-  requestAnimationFrame(render)
+class Game {
+  constructor() {
+    function render() {
+      const round = new Map(12);
+      round.draw(ctx, 0, 0);
+      requestAnimationFrame(render)
+    }
+    loader(render);
+  }
 }
 
-loader(render);
+new Game()
