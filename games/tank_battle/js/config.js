b1ca7b34cd1128fc0514886c98cabc7fa0173ab1
Your Name <you@example.com>
2022-11-12 20:59:51 +0800

    update

--- games/tank_battle/js/config.js
+++
@@ -1,8 +0,0 @@
-export const BOX_WIDTH = 26;
-export const BOX_HEIGHT = 26;
-export const BOX_SIZE = 30;
-export const TANK_SIZE = 50;
-export const BOOM_SIZE = 32;
-export const BULLET_SIZE = 8;
-export const width = BOX_SIZE * BOX_WIDTH;
-export const height = BOX_SIZE * BOX_HEIGHT;
--- games/tank_battle/js/valueToConfig.js
+++
@@ -1,8 +0,0 @@
-window.valueToConfig = {
-  1: { image: "wall" },
-  2: { image: "steel" },
-  3: { image: "grass" },
-  4: { image: "water" },
-  5: { image: "grass" },
-  9: { image: "home", height: 2, width: 2 },
-};
